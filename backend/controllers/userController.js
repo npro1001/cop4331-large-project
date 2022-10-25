3a243130e3f3a843374f40539d719c407169078e
kaydoug23 <douglaskayla23@yahoo.com>
2022-10-25 16:54:23 -0400

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
   res.status(200).json({
-    message: "hello there"
+    message: "account created"
   })
   // if (!name || !email || !password) {
   //   res.status(400)
@@ -55,20 +55,24 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
-  // Check for user email
-  const user = await User.findOne({ email })
+  res.status(200).json({
+    message: "logged in"
+  })
+  
+  // // Check for user email
+  // const user = await User.findOne({ email })
 
-  if (user && (await bcrypt.compare(password, user.password))) {
-    res.json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    })
-  } else {
-    res.status(400)
-    throw new Error('Invalid credentials')
-  }
+  // if (user && (await bcrypt.compare(password, user.password))) {
+  //   res.json({
+  //     _id: user.id,
+  //     name: user.name,
+  //     email: user.email,
+  //     token: generateToken(user._id),
+  //   })
+  // } else {
+  //   res.status(400)
+  //   throw new Error('Invalid credentials')
+  // }
 })
 
 // @desc    Get user data

8ad162ccb021c19490016359198d1176e829c2bd
npro1001 <nprocacciojr@gmail.com>
2022-10-25 16:42:31 -0400

    testing user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,41 +9,44 @@ const User = require('../models/userModel')
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
-  if (!name || !email || !password) {
-    res.status(400)
-    throw new Error('Please add all fields')
-  }
-
-  // Check if user exists
-  const userExists = await User.findOne({ email })
-
-  if (userExists) {
-    res.status(400)
-    throw new Error('User already exists')
-  }
-
-  // Hash password
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-  // Create user
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword,
+  res.status(200).json({
+    message: "hello there"
   })
+  // if (!name || !email || !password) {
+  //   res.status(400)
+  //   throw new Error('Please add all fields')
+  // }
 
-  if (user) {
-    res.status(201).json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    })
-  } else {
-    res.status(400)
-    throw new Error('Invalid user data')
-  }
+  // // Check if user exists
+  // const userExists = await User.findOne({ email })
+
+  // if (userExists) {
+  //   res.status(400)
+  //   throw new Error('User already exists')
+  // }
+
+  // // Hash password
+  // const salt = await bcrypt.genSalt(10)
+  // const hashedPassword = await bcrypt.hash(password, salt)
+
+  // // Create user
+  // const user = await User.create({
+  //   name,
+  //   email,
+  //   password: hashedPassword,
+  // })
+
+  // if (user) {
+  //   res.status(201).json({
+  //     _id: user.id,
+  //     name: user.name,
+  //     email: user.email,
+  //     token: generateToken(user._id),
+  //   })
+  // } else {
+  //   res.status(400)
+  //   throw new Error('Invalid user data')
+  // }
 })
 
 // @desc    Authenticate a user

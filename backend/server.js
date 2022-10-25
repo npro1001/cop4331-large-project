05b9e30bca53809d43b1f8f7bca6a1a740cc5874
kaydoug23 <douglaskayla23@yahoo.com>
2022-10-25 16:10:29 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,11 @@ app.use('/api/users', require('./routes/userRoutes'));
  
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
  
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
     )
   );
 } else {

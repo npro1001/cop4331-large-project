79c212d90ae5dc40ea8eda255fd44eb612b3ceb3
npro1001 <nprocacciojr@gmail.com>
2022-10-26 00:27:07 -0400

    create user functions correctly with database

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5555;
  
-// connectDB();
+connectDB();
  
 const app = express();
  

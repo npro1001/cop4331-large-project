73a5f14ffc08eaeb2d824c12f7b1f0e9f62049de
kaydoug23 <douglaskayla23@yahoo.com>
2022-10-30 20:55:04 -0400

    add username, edit login and register

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -11,6 +11,11 @@ const userSchema = mongoose.Schema(
       required: [true, 'Please add an email'],
       unique: true,
     },
+    username: {
+      type: String,
+      required: [true, 'Please add a username'],
+      unique: true,
+    },
     password: {
       type: String,
       required: [true, 'Please add a password'],

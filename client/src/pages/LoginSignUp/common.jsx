bbcbe17e817202e14b4ffde4290ec3280febf11d
canadianbees <calzenor@outlook.com>
2022-11-01 11:52:29 -0400

    added file to hold common elements

---
+++ client/src/pages/LoginSignUp/common.jsx
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+//contains the form
+export const BoxContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 10px;
+`;
+
+
+export const FormContainer = styled.form`
+    width:100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+//this is for "Forgot your passowrd"
+export const MutedLink = styled.div`
+    font-size: 13px;
+    color: #EDEDED;
+    font-weight: 500;
+    text-decoration: none;
+    align-self: center;
+`;
+
+//this is for "Login here" and "Signup here"
+export const BoldLink = styled.a`
+    font-size: 13px;
+    color: rgba(205,190,224,1);
+    font-weight: 770;
+    text-decoration: none;
+ 
+    &:not(:focus):hover {
+        filter: brightness(1.2); 
+    text-shadow: 0 0 2px #999;
+    }
+
+`;
+
+export const Input = styled.input`
+    outline: none;
+    width: 75%;
+    height: 42px;
+    border: none;
+    border-radius: 15px;
+    padding: 0px 10px;
+    border-bottom: 1px solid transparent;
+    align-self: center;
+    transition: all 100ms ease-in-out;
+    font-size: 12px;
+
+    //field will be underlined pink (color can be changed, i just chose pink at random) when selected
+    &:focus{
+        outline:none;
+        border-bottom: 5px solid pink;
+    }
+
+    &::placeholder{
+        color: black;
+    }
+
+    &::last-of-type
+    {
+        border-bottom: 1.5px solid #fffb0766;
+    }
+`;
+
+//styling for submit button
+export const SubmitButton = styled.button`
+    width: 75%;
+    padding: 11px;
+    font-size: 15px;
+    font-weight: 600;
+    border: none;
+    background: #CDBEE0;
+    border-radius: 15px;
+    cursor: pointer;
+    transition: all, 240ms ease-in-out;
+    background: rgb(205,190,224);
+    align-self: center;
+    &:hover{
+    filter: brightness(1.2);
+}
+
+   
+
+
+`;
+

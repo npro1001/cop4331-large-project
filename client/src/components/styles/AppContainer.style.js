76d7aca7bd7c2531d81c9c3bb99695915e045e4d
canadianbees <calzenor@outlook.com>
2022-11-01 11:51:47 -0400

    added AppContainer

---
+++ client/src/components/styles/AppContainer.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color:red;
+    position: absolute;
+`;

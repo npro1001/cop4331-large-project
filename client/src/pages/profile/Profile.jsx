a4abbfe64256921b01e8ea28be9786ea9983b6bb
canadianbees <calzenor@outlook.com>
2022-11-06 14:53:34 -0500

    add location of ProfileCard

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -11,7 +11,7 @@ const Profile = () => {
             <ProfileLeft/>
 
             <div className="Profile-center">
-                <ProfileCard/>
+                <ProfileCard location="profilePage"/>
                 <PostSide/>
             </div>
 

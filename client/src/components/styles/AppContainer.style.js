12084425927e4af5f78466d5c71c4f018c8e9bfd
canadianbees <calzenor@outlook.com>
2022-11-06 14:50:58 -0500

    update AppContainer

--- client/src/components/styles/AppContainer.style.js
+++ client/src/components/styles/AppContainer.style.js
@@ -3,10 +3,8 @@ import styled from 'styled-components';
 export const AppContainer = styled.div`
     width: 100%;
     height: 100%;
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    color:red;
-    position: absolute;
-`;
+    font-family: 'Ubuntu', sans-serif;
+    background: #EDEDED;
+    padding: 1rem 1rem;
+    overflow:hidden;
+    `

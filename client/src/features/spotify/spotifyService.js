6861ce7a494c750167c0c5c184c50293904f3f38
npro1001 <nprocacciojr@gmail.com>
2022-11-06 21:34:46 -0500

    dumb

--- client/src/features/spotify/spotifyService.js
+++ client/src/features/spotify/spotifyService.js
@@ -147,9 +147,9 @@ export const accessToken = connect()
 // /**
 //  * Axios global request headers
 //  */
- axios.defaults.baseURL = 'https://api.spotify.com/v1';
- axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
- axios.defaults.headers['Content-Type'] = 'application/json';
+//  axios.defaults.baseURL = 'https://api.spotify.com/v1';
+//  axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+//  axios.defaults.headers['Content-Type'] = 'application/json';
 // axios.defaults.headers['Allow-Access-Control-Origin'] = '*';
 
 const spotifyService = {

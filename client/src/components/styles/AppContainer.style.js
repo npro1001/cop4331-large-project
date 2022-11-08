b0c19c54aa2408031dc64d53216e477e0010e4e6
canadianbees <calzenor@outlook.com>
2022-11-08 13:51:29 -0500

    hid overflow

--- client/src/components/styles/AppContainer.style.js
+++ client/src/components/styles/AppContainer.style.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
     width: 100%;
-    height: 119vh;
     font-family: 'Ubuntu', sans-serif;
     background: #EDEDED;
     padding: 1rem 1rem;
+    overflow: hidden;
     `
+

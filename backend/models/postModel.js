28ea41f27f5945daf3591ddaf5d1f64f9a7b7cf0
rfarrell961 <rff961@gmail.com>
2022-11-07 19:01:24 -0500

    Update postModel.js

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 let User = require('./userModel');
 
+// TO DO: ADD PLAYLISTS AND SONGS
 const postSchema = mongoose.Schema(
     {
         author: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User',
-            required: true,
-            unique: false,
-        }
-    }
-)
+        },
+        comments: [String],
+        likes: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+        caption: String,
+        picture: String,
+    }, {timestamps: true});
 
+    module.exports = mongoose.model('Post', postSchema)

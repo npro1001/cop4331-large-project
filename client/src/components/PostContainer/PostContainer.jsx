732a4f342e88e22247dca35ee48791cfb16a4ee4
canadianbees <calzenor@outlook.com>
2022-11-10 18:21:42 -0500

    rename files

---
+++ client/src/components/PostContainer/PostContainer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import './PostContainer.css'
+import Post from "../Post/Post";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+
+const EmptyFeed = styled.h1`
+    
+    align-self: center;
+    font-size: 40px;
+    color: #391F59;
+`
+const AddOn = styled.h3`
+    align-self: center;
+    font-size: 20px;
+    color: #391F59;
+    bottom: 50px;
+    position: relative;
+`
+
+const Posts = () => {
+    const { PostData, id, author, post } = useSelector((state) => state.post)
+
+    //if there are no posts to display
+    if (PostData.length < 1) {
+        return (
+            <><EmptyFeed> No posts to display...</EmptyFeed><AddOn>Try posting something!</AddOn></>
+        )
+    }
+    return (
+        <div className="Posts">
+            {PostData.map((post, id) => {
+                return <Post data={post} id={id} />
+            })}
+        </div>
+    )
+}
+
+export default Posts

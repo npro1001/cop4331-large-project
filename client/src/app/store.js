b82fa163491a9097dcb7823d69c1dba22d3c450e
canadianbees <calzenor@outlook.com>
2022-11-11 13:01:11 -0500

    added postReducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import spotifyReducer from '../features/spotify/spotifySlice';
+import postReducer from '../features/post/postSlice';
 
 // Entire state of the application
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    spotify: spotifyReducer
+    spotify: spotifyReducer,
+    post: postReducer
   }
 });

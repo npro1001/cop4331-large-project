b59dd3efd0264a762559465582b2f303c5491ed8
canadianbees <calzenor@outlook.com>
2022-11-11 13:04:09 -0500

    refactoring, added field for comments

--- client/src/Data/PostsData.js
+++ client/src/Data/PostsData.js
@@ -1,21 +1,22 @@
 import postPic1 from '../img/postpic1.jpg'
 import postPic2 from '../img/postpic2.jpg'
 
-
 /*these are placeholder values*/
 export const PostsData = [
     {
         img: postPic1,
-        name: 'Laura',
-        desc: "Had a great time at #TheStrokesOrlando !!",
+        author: 'Laura',
+        caption: "Had a great time at #TheStrokesOrlando !!",
         likes: 37,
+        comments:"",
         liked: false
     },
     {
         img: postPic2,
-        name: 'Danny',
-        desc: "gotta put on the coding socks",
+        author: 'Danny',
+        caption: "gotta put on the coding socks",
         likes: 69,
+        comments:"",
         liked: true
     }
 ]

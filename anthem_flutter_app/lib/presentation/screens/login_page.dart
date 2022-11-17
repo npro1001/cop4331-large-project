158c39d3e6674ffda655a4f849ec1023ad2abe2b
godofthunder8756 <ahernaidan1@gmail.com>
2022-11-17 17:52:04 -0500

    cleaned up login and register

--- anthem_flutter_app/lib/presentation/screens/login_page.dart
+++ anthem_flutter_app/lib/presentation/screens/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:anthem_flutter_app/business_logic/login/login_bloc.dart';
 import 'package:anthem_flutter_app/business_logic/user_auth/user_auth_bloc.dart';
 import 'package:anthem_flutter_app/data/repositories/user_repo.dart';
@@ -36,6 +38,130 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(25),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 40),
+                Icon(Icons.music_note_sharp), //MUST BE REPLACED WITH IMAGE
+                SizedBox(height: 13),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Sign in to continue",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[400],
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: TextFormField(
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.mail, size: 30),
+                      labelText: "EMAIL",
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: TextFormField(
+                    obscureText: true,
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.lock, size: 30),
+                      labelText: "Password",
+                      labelStyle: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Forgot Passw0rd",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                  height: 55,
+                  width: double.infinity,
+                  child: FloatingActionButton.extended(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      label: Text('Login'),
+                      onPressed: () {}),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushNamed('/register');
+                      },
+                      child: Text(
+                        "Register",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  /*
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
@@ -45,6 +171,7 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
+  */
 
   @override
   void dispose() {

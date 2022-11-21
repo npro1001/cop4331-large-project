15bc446c75b6e0f2a805d96430f7a271de06b7bf
canadianbees <calzenor@outlook.com>
2022-11-21 10:01:24 -0500

    can't edit user info if your not on ur own page

--- client/src/components/infoCard/InfoCard.jsx
+++ client/src/components/infoCard/InfoCard.jsx
@@ -4,11 +4,10 @@ import { UilPen } from '@iconscout/react-unicons'
 import ProfileModal from '../profileModal/ProfileModal'
 import { logout, reset } from '../../features/auth/authSlice'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from "react"
 import { catchErrors } from "../../utils"
 import { StyledGrid } from "../styles/StyledGrid.js"
-import { spotifyLogout, connect } from '../../features/spotify/spotifySlice'
 import { getTopArtists } from "../../features/spotify/spotify.js"
 // import { getTopArtist } from "../../features/spotify/spotifyService"
 import { getTopArtist } from "../../features/spotify/spotifySlice"
@@ -17,13 +16,19 @@ import { getTopArtist } from "../../features/spotify/spotifySlice"
 
 const InfoCard = () => {
 
-    const [modalOpened, setModalOpened] = useState(false)
-    const [topArtist, setTopArtist] = useState(null);
-    const { isConnected } = useSelector((store) => store.spotify)
-
-
+    const params = useParams();
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const [modalOpened, setModalOpened] = useState(false)
+    const [topArtist, setTopArtist] = useState("None");
+    const { isConnected } = useSelector((store) => store.spotify)
+    const { user } = useSelector((state) => state.auth)
+    const [activeUser, setActiveUser] = useState({})
+    const profileUsername = params.username;
+    let profileUser;
+
+    
 
     const artistImage = "";
 
@@ -46,24 +51,33 @@ const InfoCard = () => {
         // ? Method 2 - using spotifyService.js
         // A non-serializable value was detected in an action, in the path: `payload.headers`. Value: 
         // https://redux-toolkit.js.org/usage/usage-guide#working-with-non-serializable-data
-        if(isConnected) {
-            dispatch(getTopArtist())
-            .then(response => {
-                const userTopArtist = response;
-                setTopArtist(userTopArtist.payload.data.items[0]);
-                console.log(topArtist)
-            }, [isConnected])
+        // dispatch(getTopArtist())
+        // .then(response => {
+        //     const userTopArtist = response;
+        //     setTopArtist(userTopArtist.payload.data.items[0]);
+        //     console.log(topArtist)
+        // })
+        const fetchProfileUser = async () => {
+            if (profileUsername === user.username) {
+                setActiveUser(user);
+            }
+
+            else {
+                const res = await fetch(`/api/users/${profileUsername}`, {
+                    method: 'GET',
+                    headers: { 'Accept': 'application/json' },
+                })
+                profileUser = await res.json();
+                setActiveUser(profileUser);
+            }
         }
+        fetchProfileUser()
+    }, [user]);
 
-
-    }, []);
 
     const onLogout = () => {
         dispatch(logout())
             .then(() => {
-                if(isConnected) {
-                    dispatch(spotifyLogout())
-                }
                 navigate("/", { replace: true })
             })
             .then(() => {
@@ -72,49 +86,18 @@ const InfoCard = () => {
 
     }
 
-    const onClick = (e) => {
-        e.preventDefault() 
-
-        // window.open('http://localhost:5555/api/spotify/connect', '_self');
-        // window.location.href = 'http://localhost:5555/api/spotify/connect'
-
-        dispatch(connect())
-        .then(() => {
-            // if(isError) {
-            //     toast.error(message)
-            // }
-            // navigate("/home", { replacae: true})
-            window.location.reload();
-        })
-        return true
-    }
-
-    // const params = useParams();
-
-    // const profileUserId = params.dispatchconst[profileUser , setProfileUser] = useState({})
-    // const {user} = useSelector((state)=>state.authReducer.authData)
-
-    // useEffect(()=> {
-    //     const fetchProfileUser = async()=> {
-    //         if(profileUserId === user._id) {
-    //             setProfileUser(user)
-    //         }
-    //         else{
-    //             const profileUser = await UserApi.getUserProfile(profileUserId)
-    //             setProfileUser(profileUser)
-    //         }
-    //     }
-    //     fetchProfileUser();
-    // }, [user])
-
     return (
         <div className="InfoCard">
             <div className="InfoHead">
-                <h4>Your Info</h4>
-                <div><UilPen width='2rem' height='1.2rem' onClick={() => setModalOpened(true)} /></div>
-                {/* <ProfileModal modalOpened={modalOpened}
-                    setModalOpened={setModalOpened} /> */}
+                <h4>Profile Info</h4>
+                {user.username === activeUser.username ? (<div><UilPen width='2rem' height='1.2rem' onClick={() => setModalOpened(true)} />
+                <ProfileModal modalOpened={modalOpened}
+                    setModalOpened={setModalOpened}
+                    data={user} />
+                </div>) : ("")
+                }
 
+                
             </div>
 
             <div className="info">
@@ -137,22 +120,16 @@ const InfoCard = () => {
 
                 </span>
                 <span> 
-                    { topArtist ? (
                     <div className="songrec">
                         <div>
-                        <img src={topArtist.images[0].url} alt={topArtist.name} className='songrecImg'/>
+                        {/* <img src={topArtist.images[0].url} alt={topArtist.name} className='songrecImg'/>
                             <div className="songname">
                                 <span>{topArtist.name}</span>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
-                    ) : (
-                        <div>
-                            <p> top artist not found </p>
-                            {/* <a className='button' href="http://localhost:5555/api/spotify/connect" target="_self" >Pre Connect To Spotify</a>
-                            <a className='button' href="#" target="_self" onClick={onClick}>Connect to spotify </a> */}
-                        </div>
-                        )}
+                
+
                 {/* <StyledGrid>
                 <div className="grid__item__inner">
                     <div className="grid__item__img">

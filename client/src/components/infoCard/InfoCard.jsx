a88dc45fd2a42d89dfd74339099d97c2609746a6
canadianbees <calzenor@outlook.com>
2022-11-21 10:14:13 -0500

    oops

--- client/src/components/infoCard/InfoCard.jsx
+++ client/src/components/infoCard/InfoCard.jsx
@@ -3,6 +3,7 @@ import './InfoCard.css'
 import { UilPen } from '@iconscout/react-unicons'
 import ProfileModal from '../profileModal/ProfileModal'
 import { logout, reset } from '../../features/auth/authSlice'
+import { spotifyLogout, connect } from '../../features/spotify/spotifySlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from "react"
@@ -12,7 +13,6 @@ import { getTopArtists } from "../../features/spotify/spotify.js"
 // import { getTopArtist } from "../../features/spotify/spotifyService"
 import { getTopArtist } from "../../features/spotify/spotifySlice"
 
-// import * as UserApi from '../backend/controllers/userController.js'
 
 const InfoCard = () => {
 
@@ -20,20 +20,19 @@ const InfoCard = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const [modalOpened, setModalOpened] = useState(false)
-    const [topArtist, setTopArtist] = useState("None");
     const { isConnected } = useSelector((store) => store.spotify)
     const { user } = useSelector((state) => state.auth)
+    
+    const [modalOpened, setModalOpened] = useState(false)
+    const [topArtist, setTopArtist] = useState("None");
     const [activeUser, setActiveUser] = useState({})
+
     const profileUsername = params.username;
     let profileUser;
-
-    
-
     const artistImage = "";
 
 
-    useEffect( () => {
+    useEffect(() => {
 
         // Both run multiple times?
         // https://stackoverflow.com/questions/62631053/useeffect-being-called-multiple-times
@@ -57,6 +56,15 @@ const InfoCard = () => {
         //     setTopArtist(userTopArtist.payload.data.items[0]);
         //     console.log(topArtist)
         // })
+        if (isConnected) {
+            dispatch(getTopArtist())
+                .then(response => {
+                    const userTopArtist = response;
+                    setTopArtist(userTopArtist.payload.data.items[0]);
+                    console.log(topArtist)
+                }, [isConnected])
+        }
+
         const fetchProfileUser = async () => {
             if (profileUsername === user.username) {
                 setActiveUser(user);
@@ -78,6 +86,9 @@ const InfoCard = () => {
     const onLogout = () => {
         dispatch(logout())
             .then(() => {
+                if (isConnected) {
+                    dispatch(spotifyLogout())
+                }
                 navigate("/", { replace: true })
             })
             .then(() => {
@@ -91,13 +102,13 @@ const InfoCard = () => {
             <div className="InfoHead">
                 <h4>Profile Info</h4>
                 {user.username === activeUser.username ? (<div><UilPen width='2rem' height='1.2rem' onClick={() => setModalOpened(true)} />
-                <ProfileModal modalOpened={modalOpened}
-                    setModalOpened={setModalOpened}
-                    data={user} />
+                    <ProfileModal modalOpened={modalOpened}
+                        setModalOpened={setModalOpened}
+                        data={user} />
                 </div>) : ("")
                 }
 
-                
+
             </div>
 
             <div className="info">
@@ -119,33 +130,42 @@ const InfoCard = () => {
                     <b>Top Artist</b>
 
                 </span>
-                <span> 
-                    <div className="songrec">
-                        <div>
-                        {/* <img src={topArtist.images[0].url} alt={topArtist.name} className='songrecImg'/>
-                            <div className="songname">
-                                <span>{topArtist.name}</span>
-                            </div> */}
+                <span>
+                    {/* {topArtist ? (
+                        <div className="songrec">
+                            <div>
+                                <img src={topArtist.images[0].url} alt={topArtist.name} className='songrecImg' />
+                                <img src={topArtist.images[0].url} alt={topArtist.name} className='songrecImg' />
+                                <div className="songname">
+                                    <span>{topArtist.name}</span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                
+                        
+            ) : (
+            <div>
+                <p> top artist not found </p>
+                <a className='button' href="http://localhost:5555/api/spotify/connect" target="_self" >Pre Connect To Spotify</a>
+                <a className='button' href="#" target="_self" onClick={onClick}>Connect to spotify </a>
+            </div>
+                    )}
 
-                {/* <StyledGrid>
+
+            <StyledGrid>
                 <div className="grid__item__inner">
                     <div className="grid__item__img">
                         <img src={topArtist.images[0].url} alt={topArtist.name} />
                     </div>
                     <h3 class="grid__item__name overflow-ellipsis">{topArtist.name}</h3>
-
                 </div>
-                </StyledGrid> */}
-                    
+            </StyledGrid> */}
+
                 </span>
-            </div>
+            </div >
 
             <button className='button logout-button' onClick={onLogout}>Logout</button>
 
-        </div>
+        </div >
     )
 }
 

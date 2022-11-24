480cdccba2a420a348de0b92334cf9f2c6926acd
Celina Alzenor <calzenor@outlook.com>
2022-11-24 11:04:16 -0500

    unfollow and follow works

--- client/src/components/profileCard/ProfileCard.jsx
+++ client/src/components/profileCard/ProfileCard.jsx
@@ -1,44 +1,102 @@
-import { useReducedMotion } from 'framer-motion'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import './ProfileCard.css'
-import defaultCover from '../../img/default-cover-4.jpg'
-import defaultPFP from '../../img/default-profile.png'
 import { useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import React from 'react'
+import './ProfileCard.css'
+import { follow, reset } from '../../features/interactions/intSlice';
+import defaultCover from '../../img/default-cover-4.jpg'
+import defaultPFP from '../../img/default-profile.png'
+import { useReducedMotion } from 'framer-motion'
+import styled from "styled-components";
+import { useWindowScroll } from "@mantine/hooks";
+
+const Container = styled.div`
+    position: relative;
+    align-self: center;
+    justify-content: center;
+    align-items: center;
+    width: 30%;
+    height: 40px;
+    padding:auto;
+    display:flex;
+`
+
+
+const FollowButton = styled.button`
+    width: 50%;
+    height: 100%;
+    font-size: 20px;
+    position: relative;
+    color: white;
+    border: none;
+    background: #CDBEE0;
+    border-radius: 15px;
+    cursor: pointer;
+    background: var(--purple);
+    text-shadow: 0 0 2px #999;
+    align-self: center;
+    transition: all, 240ms ease-in-out;
+
+    &:hover {
+    background: rgba(93, 48, 149, 1);
+    filter: brightness(1.2);
+}
+`
+
+
 
 const ProfileCard = ({ location }) => {
 
 
+    let isOwnProfile = true;
     const posts = useSelector((state) => state.post)
     const { user } = useSelector((state) => state.auth);
     const params = useParams();
     const navigate = useNavigate();
     const [activeUser, setActiveUser] = useState({})
-    const [following, setFollowing] = useState()
+    const [following, setFollowing] = useState(false)
     const [followers, setFollowers] = useState()
+    const [isFollowing, setIsFollowing] = useState();
     const profileUsername = params.username;
     let profileUser;
+    let loggedUser
+
 
     useEffect(() => {
         const fetchProfileUser = async () => {
 
             if (location === "homePage") {
-                setActiveUser(user);
-                setFollowing(user.following.length);
-                setFollowers(user.followers.length);
+                const res = await fetch(`/api/users/${user.username}`, {
+                    method: 'GET',
+                    headers: { 'Accept': 'application/json' },
+                })
+                loggedUser = await res.json();
+                setActiveUser(loggedUser);
+                setFollowing(loggedUser.following.length);
+                setFollowers(loggedUser.followers.length);
+
             }
 
+
+            //on a profile page
             else if (location === "profilePage") {
-                if (profileUsername === user.profileUsername) {
-                    setActiveUser(user);
-                    setFollowing(user.following.length);
-                    setFollowers(user.followers.length);
+
+                //check if its the logged in user's profile page
+                if (profileUsername === user.username) {
+                    const res = await fetch(`/api/users/${user.username}`, {
+                        method: 'GET',
+                        headers: { 'Accept': 'application/json' },
+                    })
+                    loggedUser = await res.json();
+                    setActiveUser(loggedUser);
+                    setFollowing(loggedUser.following.length);
+                    setFollowers(loggedUser.followers.length);
                 }
 
+                //logged in user is viewing someone else's page
                 else {
+                    //get the persons profile and set them as the active user
                     const res = await fetch(`/api/users/${profileUsername}`, {
                         method: 'GET',
                         headers: { 'Accept': 'application/json' },
@@ -47,11 +105,54 @@ const ProfileCard = ({ location }) => {
                     setActiveUser(profileUser);
                     setFollowing(profileUser.following.length);
                     setFollowers(profileUser.followers.length);
+
+                    //get logged in user's info to check if the user is following them
+                    const loggedRes = await fetch(`/api/users/${user.username}`, {
+                        method: 'GET',
+                        headers: { 'Accept': 'application/json' },
+                    })
+                    loggedUser = await loggedRes.json()
+                    const followingList = loggedUser.following;
+
+                    //check list to see if user is following the profile they are viewing
+                    for (let i = 0; i < followingList.length; i++) {
+                        if (followingList[i] == profileUser._id) {
+                            setIsFollowing(true);
+                        }
+                    }
                 }
             }
         }
         fetchProfileUser()
     }, [user]);
+
+
+
+    const DoFollow = async () => {
+        await fetch(`/api/users/${activeUser._id}/follow`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": "100"
+            },
+            body: JSON.stringify({ "currentUserId": user._id })
+        })
+        window.location.reload();
+    }
+
+    const DoUnFollow = async () => {
+        await fetch(`/api/users/${activeUser._id}/unfollow`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": "100"
+            },
+            body: JSON.stringify({ "currentUserId": user._id })
+        })
+        window.location.reload();
+    }
+
+
     return (
 
         <div className="ProfileCard">
@@ -70,6 +171,14 @@ const ProfileCard = ({ location }) => {
                 {location === "homePage" ? <><span>{user.name}</span> <span>@{user.username}</span></> : <><span>{activeUser.name}</span> <span>@{activeUser.username}</span></>}
 
             </div>
+            {location === "profilePage" && isFollowing && (activeUser.username != user.username) ?
+                <Container>
+                    <FollowButton onClick={DoUnFollow}>Unfollow</FollowButton>
+                </Container> : ""}
+            {location === "profilePage" && !isFollowing && (activeUser.username != user.username) ? <Container>
+                <FollowButton onClick={DoFollow}>Follow</FollowButton>
+            </Container> : ""}
+
 
             <div className="followStatus">
                 <hr />

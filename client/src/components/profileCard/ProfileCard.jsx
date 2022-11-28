a2e93a3fa6dc254fbb80b769d14b6f083f548494
rfarrell961 <rff961@gmail.com>
2022-11-28 18:42:58 -0500

    Commented out line that makes everything break

--- client/src/components/profileCard/ProfileCard.jsx
+++ client/src/components/profileCard/ProfileCard.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import React from 'react'
 import './ProfileCard.css'
-import { follow, reset } from '../../features/interactions/intSlice';
+// import { follow, reset } from '../../features/interactions/intSlice';
 import defaultCover from '../../img/default-cover-4.jpg'
 import defaultPFP from '../../img/default-profile.png'
 import { useReducedMotion } from 'framer-motion'

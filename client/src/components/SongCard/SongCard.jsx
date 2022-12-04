bd521e4531ab30e4f1675a8de9c6ba413464fcfd
canadianbees <calzenor@outlook.com>
2022-12-04 16:15:47 -0500

    song card plays user's anthem when clicked (WOW!!)

--- client/src/components/SongCard/SongCard.jsx
+++ client/src/components/SongCard/SongCard.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
 import './SongCard.css'
 import styled from "styled-components";
+import { Howl } from "howler"
+import { useState } from 'react';
 
+var audio = null;
 // id?
-const SongCard = ({name, artist1, image, url}) => {
+const SongCard = ({ name, artist1, image, url }) => {
 
+    const soundPlay = () => {
+
+        //check if its already playing, if it is then stop
+        if (audio != null) {
+           
+            audio.stop();
+            audio.unload();
+            audio = null;
+        }
+
+        //if not play the song
+        else{
+            audio = new Howl({
+                src: [url],
+                html5: true
+            })
+            audio.play();
+            audio.fade(2.0,0);
+        }
+        
+        
+    }
+
+    
     return (
         <div className="SongCard">
             <div className="song">
-                <div>
-                <img src={image} alt="" className='songImg'/>
-                        <div className="songnameInfo">
-                                <span>{name}</span>
-                                <span>{artist1}</span>
-                        </div>
+                <div onClick={soundPlay}>
+                    <img src={image} alt="" className='songImg' />
+                    <div className="songnameInfo">
+                        <span>{name}</span>
+                        <span>{artist1}</span>
+                    </div>
                 </div>
-                {/*<button className='button sr-button'></button>*/}
             </div>
         </div>
     )

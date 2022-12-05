2b791f5358a2cec374b07ad4daf17c7d19856c51
canadianbees <calzenor@outlook.com>
2022-12-05 12:57:39 -0500

    comment out placeholder data

--- client/src/Data/PostsData.js
+++ client/src/Data/PostsData.js
@@ -3,20 +3,20 @@ import postPic2 from '../img/postpic2.jpg'
 
 /*these are placeholder values*/
 export const PostsData = [
-    {
-        img: postPic1,
-        author: 'Laura',
-        caption: "Had a great time at #TheStrokesOrlando !!",
-        likes: 37,
-        comments:"",
-        liked: false
-    },
-    {
-        img: postPic2,
-        author: 'Danny',
-        caption: "gotta put on the coding socks",
-        likes: 69,
-        comments:"",
-        liked: true
-    }
+    // {
+    //     img: postPic1,
+    //     author: 'Laura',
+    //     caption: "Had a great time at #TheStrokesOrlando !!",
+    //     likes: 37,
+    //     comments:"",
+    //     liked: false
+    // },
+    // {
+    //     img: postPic2,
+    //     author: 'Danny',
+    //     caption: "gotta put on the coding socks",
+    //     likes: 69,
+    //     comments:"",
+    //     liked: true
+    // }
 ]

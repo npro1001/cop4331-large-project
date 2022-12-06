c01bf099e7b95304972d09f7c1cd1ff617601c94
godofthunder8756 <64430471+godofthunder8756@users.noreply.github.com>
2022-12-06 12:06:53 -0500

    made feed

---
+++ anthem_flutter_app/lib/presentation/screens/feed.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../business_logic/user_auth/user_auth_bloc.dart';
+
+class Feed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEDF0F0F6), //Instagram color WHAAA???
+      body:
+          ListView(physics: AlwaysScrollableScrollPhysics(), children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Anthem",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32.0,
+                ),
+              ),
+              Row(children: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.queue_music_sharp),
+                    iconSize: 30.0,
+                    onPressed: () => print("AnthemTV")),
+                SizedBox(width: 16.0),
+                Container(
+                  width: 35.0,
+                  child: IconButton(
+                      icon: Icon(Icons.send),
+                      iconSize: 30.0,
+                      onPressed: () => print("DMs")),
+                )
+              ])
+            ],
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          height: 100,
+          color: Colors.red,
+        ),
+      ]),
+    );
+  }
+}

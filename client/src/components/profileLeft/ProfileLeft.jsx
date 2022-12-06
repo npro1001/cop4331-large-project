28a16f0a4b69c46c620f7106affa0f8c08d49f49
rachel <schwarz.rachel.e@gmail.com>
2022-12-06 11:44:59 -0500

    song recs dont show on other user profile

--- client/src/components/profileLeft/ProfileLeft.jsx
+++ client/src/components/profileLeft/ProfileLeft.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import {React, useEffect, useState } from "react";
 import InfoCard from "../infoCard/InfoCard";
 import LogoSearch from "../logoSearch/LogoSearch";
 import SongRecsCard from "../songRecsCard/SongRecsCard";
 import './ProfileLeft.css'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom';
 
 const ProfileLeft = () => {
+    const user = useSelector((state) => state.auth.user)
+    const [activeUser, setActiveUser] = useState({})
+    const params = useParams();
+    const profileUsername = params.username;
+    let profileUser;
+
+    const fetchProfileUser = async () => {
+
+        //if users profile
+        if (profileUsername === user.username) {
+            setActiveUser(user);
+        }
+
+        //logged in user is viewing someone else's page
+        else {
+            //get the persons profile and set them as the active user
+            const res = await fetch(`/api/users/${profileUsername}`, {
+                method: 'GET',
+                headers: { 'Accept': 'application/json' },
+            })
+            profileUser = await res.json();
+            setActiveUser(profileUser);
+        }
+    }
+
+    useEffect(() => {
+        fetchProfileUser()
+    },[user]);
+
+
     return (
         <div className="ProfileLeft">
             <LogoSearch/> 
             <InfoCard/>
-            <SongRecsCard/>
+            {(user.username !== activeUser.username)
+                ? 
+                ""
+                : <SongRecsCard/>}
+            
         </div>
     )
 }

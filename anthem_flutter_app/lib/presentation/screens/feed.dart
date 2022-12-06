329feb6c36140b534702e8cf64a7485444d2a855
godofthunder8756 <64430471+godofthunder8756@users.noreply.github.com>
2022-12-06 14:03:05 -0500

    multiple posts

--- anthem_flutter_app/lib/presentation/screens/feed.dart
+++ anthem_flutter_app/lib/presentation/screens/feed.dart
@@ -7,6 +7,109 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../business_logic/user_auth/user_auth_bloc.dart';
 
 class Feed extends StatelessWidget {
+  Widget _buildPost(int index) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+      child: Container(
+        width: double.infinity,
+        height: 560.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black45,
+                            offset: Offset(0, 2),
+                            blurRadius: 6.0,
+                          ),
+                        ],
+                      ),
+                      child: CircleAvatar(
+                        child: ClipOval(
+                            child: Image(
+                          height: 50,
+                          width: 50,
+                          image: AssetImage("assets/lib/dogpfp.png"), //USER PFP
+                          fit: BoxFit.cover,
+                        )),
+                      ),
+                    ),
+                    title: Text(
+                      "KINGSLAYER69",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text("At Your Mom's House"),
+                    trailing: IconButton(
+                      icon: Icon(Icons.more_horiz),
+                      color: Colors.black,
+                      onPressed: () => print("More Options"),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: double.infinity,
+                    height: 400,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black45,
+                          offset: Offset(0, 5),
+                          blurRadius: 8.0,
+                        ),
+                      ],
+                      image: DecorationImage(
+                        image: AssetImage("lib/inevitable_shitpost.png"),
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.favorite_border),
+                              iconSize: 30.0,
+                              onPressed: () => print("Like Post"),
+                            ),
+                            Text(
+                              '69,696,969',
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,104 +151,8 @@ class Feed extends StatelessWidget {
             height: 100,
             color: Colors.grey,
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-            child: Container(
-              width: double.infinity,
-              height: 560.0,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25.0),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: Column(
-                      children: <Widget>[
-                        ListTile(
-                          leading: Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black45,
-                                  offset: Offset(0, 2),
-                                  blurRadius: 6.0,
-                                ),
-                              ],
-                            ),
-                            child: CircleAvatar(
-                              child: ClipOval(
-                                  child: Image(
-                                height: 50,
-                                width: 50,
-                                image: AssetImage(
-                                    "assets/lib/dogpfp.png"), //USER PFP
-                                fit: BoxFit.cover,
-                              )),
-                            ),
-                          ),
-                          title: Text(
-                            "KINGSLAYER69",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Text("At Your Mom's House"),
-                          trailing: IconButton(
-                            icon: Icon(Icons.more_horiz),
-                            color: Colors.black,
-                            onPressed: () => print("More Options"),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          width: double.infinity,
-                          height: 400,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black45,
-                                offset: Offset(0, 5),
-                                blurRadius: 8.0,
-                              ),
-                            ],
-                            image: DecorationImage(
-                              image: AssetImage("lib/inevitable_shitpost.png"),
-                              fit: BoxFit.fitWidth,
-                            ),
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Row(
-                              children: <Widget>[
-                                IconButton(
-                                  icon: Icon(Icons.favorite_border),
-                                  iconSize: 30.0,
-                                  onPressed: () => print("Like Post"),
-                                ),
-                                Text(
-                                  '69,696,969',
-                                  style: TextStyle(
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          _buildPost(0),
+          _buildPost(1),
         ],
       ),
     );

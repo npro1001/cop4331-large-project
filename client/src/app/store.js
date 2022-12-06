e9ba945bf5b85ea9d25e9cecd62dcafa6c969507
npro1001 <nprocacciojr@gmail.com>
2022-12-06 11:20:36 -0500

    possible golden egg for state management, but unstable

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -4,6 +4,10 @@ import spotifyReducer from '../features/spotify/spotifySlice';
 import postReducer from '../features/post/postSlice';
 
 // Entire state of the application
+// const persistedState = localStorage.getItem('user') 
+//                        ? JSON.parse(localStorage.getItem('user'))
+//                        : {}
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
@@ -11,4 +15,9 @@ export const store = configureStore({
     post: postReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false,}),
+  // persistedState,
 });
+
+// store.subscribe(()=>{
+//   localStorage.setItem('user', JSON.stringify(store.auth)) //! Dont do store.auth.user
+// })

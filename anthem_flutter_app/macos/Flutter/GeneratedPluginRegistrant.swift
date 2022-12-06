713d11b7ee89aab86123bf59ac040b38d524923b
godofthunder8756 <64430471+godofthunder8756@users.noreply.github.com>
2022-12-06 11:03:39 -0500

    bruh

--- anthem_flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ anthem_flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import flutter_secure_storage_macos
+import just_audio
 import path_provider_macos
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }

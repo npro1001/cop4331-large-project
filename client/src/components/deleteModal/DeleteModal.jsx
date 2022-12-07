bdfb4671fef404f8c13cde7574c8aa9e7cccaec0
rachel <schwarz.rachel.e@gmail.com>
2022-12-07 00:15:15 -0500

    delete actually works now

--- client/src/components/deleteModal/DeleteModal.jsx
+++ client/src/components/deleteModal/DeleteModal.jsx
@@ -1,8 +1,23 @@
 import { Modal, useMantineTheme } from '@mantine/core'
+import { useParams } from 'react-router-dom';
 import './DeleteModal.css'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function DeleteModal({modalOpened, setModalOpened, post}) {
     const theme = useMantineTheme();
+    const [activeUser, setActiveUser] = useState({})
+    const params = useParams();
+    const { user } = useSelector((state) => state.auth);
+
+    const fetchProfileUser = async () => {
+        setActiveUser(user);
+    }
+
+    useEffect(() => {
+        fetchProfileUser()
+    },[user]);
 
     const deletePost = async () => {
         const postId = post.id
@@ -10,7 +25,7 @@ function DeleteModal({modalOpened, setModalOpened, post}) {
 
         await fetch(`/api/post/delete`, {
             method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${user.token}` },
             body: JSON.stringify({postId})
         }).then(response => {
             return response.json()
@@ -19,7 +34,7 @@ function DeleteModal({modalOpened, setModalOpened, post}) {
             console.log(data) 
         );
         setModalOpened(false)
-        window.location.reload()
+        //window.location.reload()
     }
 
     return(
